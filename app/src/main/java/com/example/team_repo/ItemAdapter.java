c1f24ad0a0942c7f3ab9b9ea55bdf0e78384a1b7
Kyle <kylemestacio@gmail.com>
2023-11-04 00:44:21 -0600

    Fixed text formatting on homepage item list

--- app/src/main/java/com/example/team_repo/ItemAdapter.java
+++ app/src/main/java/com/example/team_repo/ItemAdapter.java
@@ -41,7 +41,7 @@ public class ItemAdapter extends ArrayAdapter<Item> {
         TextView item_value = view.findViewById(R.id.itemValue);
         TextView item_purchase_date = view.findViewById(R.id.itemPurchaseDate);
 
-        // Set the TextViews and ImageView views to the item's details
+        // Set the ImageView to the item's photo (or a default image)
         if (item.getImage() != null) {
             // TODO: display image
             item_image.setImageResource(R.drawable.baseline_image_not_supported_24);
@@ -50,10 +50,31 @@ public class ItemAdapter extends ArrayAdapter<Item> {
             item_image.setImageResource(R.drawable.baseline_image_not_supported_24);
         }
 
-        item_name.setText(item.getName());
-        item_make.setText(item.getMake());
-        item_value.setText(String.format("%.2f", item.getValue()));
+        // Set the item's name
+        if (item.getName().length() <= 12) {
+            item_name.setText(item.getName());
+        }
+        else {
+            item_name.setText(item.getName().substring(0, 11) + "...");
+        }
 
+        // Set the item's make
+        if (item.getMake().length() <= 15) {
+            item_make.setText(item.getMake());
+        }
+        else {
+            item_make.setText(item.getMake().substring(0, 14) + "...");
+        }
+
+        // Set the item's value
+        if (String.format("%.2f", item.getValue()).length() <= 15) {
+            item_value.setText(String.format("%.2f", item.getValue()));
+        }
+        else {
+            item_value.setText(String.format("%.2f", item.getValue()).substring(0, 14) + "...");
+        }
+
+        // Set the item's date
         SimpleDateFormat date_format = new SimpleDateFormat("YYYY-MM-DD");
         String date = date_format.format(item.getDate());
         item_purchase_date.setText(date);
