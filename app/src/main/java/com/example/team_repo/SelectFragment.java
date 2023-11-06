87f5735409453d89a0fad7a43443be54bdcb3ea7
User20220908 <yurong3@ualberta.ca>
2023-11-05 22:48:57 -0700

    Create SelectFragment.java

---
+++ app/src/main/java/com/example/team_repo/SelectFragment.java
@@ -0,0 +1,30 @@
+package com.example.team_repo;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class SelectFragment {
+
+    //Select from list
+    /*Button select_button = view.findViewById(R.id.selectItemsButton);
+        select_button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            item_list_view.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        }
+    });
+
+        item_list_view.setOnItemClickListener((parent, view1, position, id) ->
+
+    {
+        boolean checked = item_list.get(position).checked;
+        if (!checked) {
+            item_list.get(position).checked = true;
+        } else {
+            item_list.get(position).checked = false;
+        }
+        itemAdapter.notifyDataSetChanged();
+    });*/
+}
