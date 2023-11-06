7719e2fa2842915c15870d51a055c33e337424c5
User20220908 <yurong3@ualberta.ca>
2023-11-06 12:44:14 -0700

    Update SelectFragment.java

--- app/src/main/java/com/example/team_repo/SelectFragment.java
+++ app/src/main/java/com/example/team_repo/SelectFragment.java
@@ -1,6 +1,9 @@
 package com.example.team_repo;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,30 +11,79 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.ArrayList;
 
 public class SelectFragment extends Fragment {
 
     private ItemAdapter itemAdapter;
-    private ItemList item_list;
+    private ArrayList<Item> item_list;
     private ListView item_list_view;
-    //private FirebaseFirestore db;
-    //private CollectionReference citiesRef;
+    private FirebaseFirestore db;
+    private CollectionReference itemsRef;
 
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_select, container, false);
 
         // Attach the items in the item list to the adapter
+        db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("items").document("test1");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document_snap = task.getResult();
+                    if (document_snap.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document_snap.getData());
+                        item_list = (ArrayList<Item>) document_snap.get("item_list");
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>(){
+            @Override
+            public void onEvent (@Nullable DocumentSnapshot snapshot,
+                                 @Nullable FirebaseFirestoreException e){
+                if (e != null) {
+                    Log.w(TAG, "Listen failed.", e);
+                    return;
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    Log.d(TAG, "Current data: " + snapshot.getData());
+                } else {
+                    Log.d(TAG, "Current data: null");
+                }
+            }
+        });
+
         item_list_view = view.findViewById(R.id.selectPageListView);
-        itemAdapter = new ItemAdapter(this.getContext(), item_list.getList());
+        itemAdapter = new ItemAdapter(this.getContext(), item_list);
         item_list_view.setAdapter(itemAdapter);
 
         return view;
     }
+
 
     //Select from list
     /*Button select_button = view.findViewById(R.id.selectItemsButton);
