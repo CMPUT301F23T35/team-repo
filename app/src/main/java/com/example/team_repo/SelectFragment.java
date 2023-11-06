da9a66fb4b84f6b35d097e84d6a8a8e702ad9446
User20220908 <yurong3@ualberta.ca>
2023-11-05 23:16:34 -0700

    Update SelectFragment.java

--- app/src/main/java/com/example/team_repo/SelectFragment.java
+++ app/src/main/java/com/example/team_repo/SelectFragment.java
@@ -1,10 +1,37 @@
 package com.example.team_repo;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class SelectFragment {
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class SelectFragment extends Fragment {
+
+    private ItemAdapter itemAdapter;
+    private ItemList item_list;
+    private ListView item_list_view;
+    private FirebaseFirestore db;
+    private CollectionReference citiesRef;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_select, container, false);
+
+        // Attach the items in the item list to the adapter
+        item_list_view = view.findViewById(R.id.selectPageListView);
+        itemAdapter = new ItemAdapter(this.getContext(), item_list.getList());
+        item_list_view.setAdapter(itemAdapter);
+
+        return view;
+    }
 
     //Select from list
     /*Button select_button = view.findViewById(R.id.selectItemsButton);
