4ad9d9a2ddff7ea9296ab90252369292c91b05c0
raf <94569012+raf-pas@users.noreply.github.com>
2023-11-07 15:48:54 -0700

    - Fixed some bugs - Added comments to improve readability

--- app/src/main/java/com/example/team_repo/CameraFragment.java
+++ app/src/main/java/com/example/team_repo/CameraFragment.java
@@ -24,7 +24,9 @@ public class CameraFragment extends Fragment {
         activityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.RequestPermission(),
                 result -> {
+                    // Callback to handle the result of camera permission request.
                     if (result) {
+                        // If permission is granted, start the camera.
                         if (cameraHandler != null) {
                             cameraHandler.startCamera();
                         }
@@ -35,19 +37,25 @@ public class CameraFragment extends Fragment {
 
     @Nullable
     @Override
+    // Called to have the fragment instantiate its user interface view.
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment.
         View view = inflater.inflate(R.layout.fragment_camera, container, false);
 
+        // Initialize the UI components.
         previewView = view.findViewById(R.id.cameraPreview);
         captureButton = view.findViewById(R.id.capture);
 
+        // Initialize the camera handler to manage camera operations.
         cameraHandler = new CameraHandler(getActivity(), previewView, captureButton, activityResultLauncher);
+        // Request camera permissions as soon as the view is created.
         cameraHandler.requestCameraPermission();
 
         return view;
     }
 
     @Override
+    // When the fragment is resumed, ensure the camera starts if permissions are granted.
     public void onResume() {
         super.onResume();
         if (cameraHandler != null) {
@@ -56,9 +64,13 @@ public class CameraFragment extends Fragment {
     }
 
     @Override
+    // When the fragment is paused, stop the camera to release resources.
     public void onPause() {
         super.onPause();
-        // Optionally unbind the camera resources on pause
+        if (cameraHandler != null) {
+            cameraHandler.stopCamera();
+        }
     }
+
 }
 
--- app/src/main/java/com/example/team_repo/CameraHandler.java
+++ app/src/main/java/com/example/team_repo/CameraHandler.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.view.View;
@@ -14,7 +13,6 @@ import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.camera.core.AspectRatio;
-import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
@@ -26,152 +24,113 @@ import androidx.fragment.app.FragmentActivity;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
 
 public class CameraHandler {
     private final Context context;
     private final PreviewView previewView;
-    private final ImageButton capture;
-    private int cameraFacing = CameraSelector.LENS_FACING_BACK;
+    private final ImageButton captureButton;
     private final ActivityResultLauncher<String> activityResultLauncher;
+    private boolean isCameraStarted = false;
 
-    public CameraHandler(Context context, PreviewView previewView, ImageButton capture, ActivityResultLauncher<String> activityResultLauncher) {
+    public CameraHandler(Context context, PreviewView previewView, ImageButton captureButton, ActivityResultLauncher<String> activityResultLauncher) {
         this.context = context;
         this.previewView = previewView;
-        this.capture = capture;
+        this.captureButton = captureButton;
         this.activityResultLauncher = activityResultLauncher;
     }
 
+    // Requests camera permission using the provided ActivityResultLauncher.
     public void requestCameraPermission() {
         activityResultLauncher.launch(Manifest.permission.CAMERA);
     }
 
+    // Starts the camera preview if it hasn't been started already.
     public void startCamera() {
-        int aspectRatio = aspectRatio(previewView.getWidth(), previewView.getHeight());
-        ListenableFuture<ProcessCameraProvider> listenableFuture = ProcessCameraProvider.getInstance(context);
-
-        listenableFuture.addListener(() -> {
+        if (isCameraStarted) {
+            return;
+        }
+        // Future to obtain the camera provider which manages the camera lifecycle.
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(context);
+        cameraProviderFuture.addListener(() -> {
+            // Attempt to start the camera and bind its lifecycle to the FragmentActivity.
             try {
-                ProcessCameraProvider cameraProvider = (ProcessCameraProvider) listenableFuture.get();
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
 
-                Preview preview = new Preview.Builder().setTargetAspectRatio(aspectRatio).build();
+                Preview preview = new Preview.Builder().build();
 
                 ImageCapture imageCapture = new ImageCapture.Builder()
                         .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                        .setTargetRotation(((FragmentActivity) context).getWindowManager().getDefaultDisplay().getRotation())
                         .build();
 
                 CameraSelector cameraSelector = new CameraSelector.Builder()
-                        .requireLensFacing(cameraFacing).build();
-
-                cameraProvider.unbindAll();
-
-                Camera camera = cameraProvider.bindToLifecycle((FragmentActivity) context, cameraSelector, preview, imageCapture);
-
-                capture.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                            activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-                        }
-                        takePicture(imageCapture);
-                    }
-                });
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build();
 
                 preview.setSurfaceProvider(previewView.getSurfaceProvider());
+                cameraProvider.unbindAll();
+                cameraProvider.bindToLifecycle((FragmentActivity) context, cameraSelector, preview, imageCapture);
+
+                captureButton.setOnClickListener(v -> takePicture(imageCapture));
+
+                isCameraStarted = true;
             } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
+                Toast.makeText(context, "Error starting camera: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                Thread.currentThread().interrupt();
             }
         }, ContextCompat.getMainExecutor(context));
     }
-    public void takePicture(final ImageCapture imageCapture) {
-        final String filename = System.currentTimeMillis() + ".jpg";
-        final ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE, filename);
-        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-
-        final ContentResolver resolver = context.getContentResolver();
-        Uri uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-
-        if (uri == null) {
-            ((FragmentActivity) context).runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(context, "Failed to create new MediaStore record.", Toast.LENGTH_SHORT).show();
-                }
-            });
+    // Stops the camera preview if it is currently running.
+    public void stopCamera() {
+        if (!isCameraStarted) {
             return;
         }
 
-        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(new File(context.getExternalCacheDir(), filename)).build();
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(context);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                cameraProvider.unbindAll();
+                isCameraStarted = false;
+            } catch (ExecutionException | InterruptedException e) {
+                Toast.makeText(context, "Error stopping camera: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                Thread.currentThread().interrupt();
+            }
+        }, ContextCompat.getMainExecutor(context));
+    }
 
-        imageCapture.takePicture(outputFileOptions, Executors.newCachedThreadPool(), new ImageCapture.OnImageSavedCallback() {
+    // Captures an image and saves it to the external storage
+    private void takePicture(ImageCapture imageCapture) {
+        // Content values to describe the saved image's metadata.
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, System.currentTimeMillis());
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+
+        // Configuration for how and where the image should be saved.
+        ImageCapture.OutputFileOptions outputFileOptions =
+                new ImageCapture.OutputFileOptions.Builder(
+                        context.getContentResolver(),
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        contentValues
+                ).build();
+
+        // Initiates an asynchronous image capture which will be processed in the provided executor.
+        imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(context), new ImageCapture.OnImageSavedCallback() {
+            // Callback for when the image has been successfully saved.
             @Override
             public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
-                File file = new File(context.getExternalCacheDir(), filename);
-                try {
-                    InputStream inputStream = new FileInputStream(file);
-                    OutputStream outputStream = resolver.openOutputStream(uri);
-
-                    if (outputStream != null) {
-                        byte[] buffer = new byte[4 * 1024];
-                        int read;
-                        while ((read = inputStream.read(buffer)) != -1) {
-                            outputStream.write(buffer, 0, read);
-                        }
-                        inputStream.close();
-                        outputStream.close();
-
-                        file.delete();
-                        ((FragmentActivity) context).runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(context, "Image saved at: " + uri.toString(), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    } else {
-                        file.delete();
-                        ((FragmentActivity) context).runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(context, "Failed to save image.", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                startCamera();
+                Uri savedUri = outputFileResults.getSavedUri();
+                String msg = "Photo capture succeeded: " + savedUri;
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
             }
 
+
+            // Callback for when there is an error during image capture.
             @Override
             public void onError(@NonNull ImageCaptureException exception) {
-                ((FragmentActivity) context).runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(context, "Failed to save: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-                startCamera();
+                Toast.makeText(context, "Photo capture failed: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
-
-
-    private int aspectRatio(int width, int height) {
-        double previewRatio = (double) Math.max(width, height) / Math.min(width, height);
-        if (Math.abs(previewRatio - 4.0 / 3.0) <= Math.abs(previewRatio - 16.0 / 9.0)) {
-            return AspectRatio.RATIO_4_3;
-        }
-        return AspectRatio.RATIO_16_9;
-    }
 }
-
 
