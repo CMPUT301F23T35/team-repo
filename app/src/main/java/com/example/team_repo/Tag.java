f3907635d55f660d6bded8d05a1d38557c8105b0
User20220908 <yurong3@ualberta.ca>
2023-11-10 11:21:52 -0700

    Update Tag.java

--- app/src/main/java/com/example/team_repo/Tag.java
+++ app/src/main/java/com/example/team_repo/Tag.java
@@ -4,26 +4,44 @@ import java.io.Serializable;
 
 public class Tag implements Serializable {
     private String tagString;
-
     private boolean isSelected = false;
 
+    /**
+     * Empty constructor
+     */
     Tag(){
 
     }
+
+    /**
+     * Constructor for a tag with tag string provided
+     * @param tag
+     */
     Tag(String tag){
         this.tagString  = tag;
     }
 
+    /**
+     * Return the string content of a tag
+     * @return
+     */
     String getTagString(){
         return this.tagString;
     }
 
+    /**
+     * Check if a tag is selected
+     * @return
+     */
     public boolean isSelected() {
         return isSelected;
     }
 
+    /**
+     * Change a tag's state to selceted
+     * @param selected
+     */
     public void setSelected(boolean selected) {
         isSelected = selected;
     }
-
 }
