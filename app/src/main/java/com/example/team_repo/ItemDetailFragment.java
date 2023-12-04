6238581150fd313561fb927482bafda21870e416
Maanit Pratap Singh <maanitpr@ualberta.ca>
2023-12-03 19:31:46 -0700

    Fixed Null Exception Error in ItemDetailFragment
    Fixed Null Exception Error in ItemDetailFragment

--- app/src/main/java/com/example/team_repo/ItemDetailFragment.java
+++ app/src/main/java/com/example/team_repo/ItemDetailFragment.java
@@ -192,7 +192,7 @@ public class ItemDetailFragment extends Fragment {
                         public void run() {
                             imageNameList = imageNames;
                             Context context = getContext(); // check if current context is alive
-                            if (context != null) {
+                            if (context != null && bitmaps != null) {
                                 if (bitmaps.isEmpty()) {
                                     ImageView imageView = new ImageView(context);
                                     imageView.setImageResource(R.drawable.baseline_image_not_supported_24);
